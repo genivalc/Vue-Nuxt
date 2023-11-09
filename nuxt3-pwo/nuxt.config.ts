ca3cc406e8bbf74bd5fe1df541ab8ef7b6dde9fc
genivalc <genivalcandeiadon.neto@gmail.com>
2023-11-09 18:40:20 -0300

    feat configurention pwa

--- nuxt3-pwo/nuxt.config.ts
+++ nuxt3-pwo/nuxt.config.ts
@@ -1,4 +1,44 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    "@vite-pwa/nuxt"
+  ],
+  pwa: {
+    manifest: {
+      name: "Nuxt3 PWA testing GEnival",
+      short_name: "Nuxt3 PWA testing GEnival",
+      description: "testing nuxt3 PWA",
+      icons: [
+        {
+          src: 'icons/icon_64x64.png',
+          sizes: '64x64',
+          type: 'image/png'
+        },
+        {
+          src: 'icons/icon_144x144.png',
+          sizes: '144x144',
+          type: 'image/png'
+        },
+        {
+          src: 'icons/icon_192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'icons/icon_512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        },
+      ]
+    },
+    workbox: {
+      navigateFallback: '/',
+    },
+    devOptions:{
+      enabled: true,
+      type: "module"
+    }
+  },
+
 })
