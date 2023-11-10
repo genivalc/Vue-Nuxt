dfea6a963da9d9a595b756b7bb65b00ab1bd9a4b
genivalc <genivalcandeiadon.neto@gmail.com>
2023-11-10 17:03:39 -0300

    fixs

--- nuxt3-pwo/nuxt.config.ts
+++ nuxt3-pwo/nuxt.config.ts
@@ -1,46 +1,44 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    "@vite-pwa/nuxt",
-    "@nuxtjs/tailwindcss"
-  ],
+  css: ['@/assets/css/tailwind.css'],
+  modules: ['@vite-pwa/nuxt', '@nuxtjs/tailwindcss', 'nuxt-server-utils', '@sidebase/nuxt-auth'],
   pwa: {
     manifest: {
-      name: "Nuxt3 PWA testing GEnival",
-      short_name: "Nuxt3 PWA testing GEnival",
-      description: "testing nuxt3 PWA",
+      name: 'Nuxt3 PWA testing GEnival',
+      short_name: 'Nuxt3 PWA testing GEnival',
+      description: 'testing nuxt3 PWA',
       icons: [
         {
           src: 'icons/icon_64x64.png',
           sizes: '64x64',
-          type: 'image/png'
+          type: 'image/png',
         },
         {
           src: 'icons/icon_144x144.png',
           sizes: '144x144',
-          type: 'image/png'
+          type: 'image/png',
         },
         {
           src: 'icons/icon_192x192.png',
           sizes: '192x192',
-          type: 'image/png'
+          type: 'image/png',
         },
         {
           src: 'icons/icon_512x512.png',
           sizes: '512x512',
-          type: 'image/png'
+          type: 'image/png',
         },
-      ]
+      ],
     },
     workbox: {
       navigateFallback: '/',
+      globDirectory: 'C:/dev/Vue-Nuxt/nuxt3-pwo/.nuxt/dev-sw-dist',
+      globPatterns: ['**/*.{js,css,html}'],
+      globIgnores: ['**/node_modules/**/*', 'sw.js', 'workbox-*.js'],
     },
-    devOptions:{
+    devOptions: {
       enabled: true,
-      type: "module"
-    }
+      type: 'module',
+    },
   },
-  
-
 })
