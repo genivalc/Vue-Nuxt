dc5a4156a81537df6828b2b3eda9d41d4f7b96f8
genivalc <genivalcandeiadon.neto@gmail.com>
2023-11-13 10:38:53 -0300

    feat created layout and colormode

---
+++ nuxt3-pwo/layouts/default.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <header>
+      <div>
+        <h1>Color mode: {{ $colorMode.value }}</h1>
+        <select v-model="$colorMode.preference">
+          <option value="system">System</option>
+          <option value="light">Light</option>
+          <option value="dark">Dark</option>
+          <option value="sepia">Sepia</option>
+        </select>
+      </div>
+    </header>
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+<script setup>
+const colorMode = useColorMode()
+
+console.warn(colorMode.preference)
+</script>
+
+<style>
+body {
+  background-color: #fff;
+  color: rgba(0, 0, 0, 0.8);
+}
+.dark-mode body {
+  background-color: #091a28;
+  color: #ebf4f1;
+}
+.sepia-mode body {
+  background-color: #f1e7d0;
+  color: #433422;
+}
+</style>
