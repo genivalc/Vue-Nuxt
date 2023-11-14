7fb8457a9c58efc1a80c7dbd09d0d44cbc2028f3
genivalc <genivalcandeiadon.neto@gmail.com>
2023-11-14 17:31:26 -0300

    fix header

--- nuxt3-pwo/components/Headers.vue
+++ nuxt3-pwo/components/Headers.vue
@@ -3,41 +3,47 @@
   <header class="bg-gray-800 p-4">
     <div class="container mx-auto flex justify-between items-center">
       <div>
-        <img src="/logo.png" alt="Logo" class="h-12" />
+        <img src="/logo.png" alt="Logo" class="h-8 w-auto" />
       </div>
-      <div class="hidden md:flex items-center space-x-4">
-        <input type="text" placeholder="Pesquisar..." class="px-2 py-1 border rounded-md" />
-        <button class="bg-blue-500 text-white px-4 py-2 rounded-md">Pesquisar</button>
-      </div>
-      <div class="md:hidden">
+      <div class="lg:hidden">
         <button @click="toggleMobileMenu" class="text-white">
           <svg
+            class="h-6 w-6"
             fill="none"
             stroke="currentColor"
             stroke-linecap="round"
             stroke-linejoin="round"
             stroke-width="2"
             viewBox="0 0 24 24"
-            class="w-6 h-6"
           >
             <path d="M4 6h16M4 12h16m-7 6h7"></path>
           </svg>
         </button>
       </div>
+      <div class="hidden lg:flex space-x-4">
+        <!-- Seus itens de menu aqui -->
+        <a href="#" class="text-white">Homepage</a>
+        <a href="#" class="text-white">Livros</a>
+        <a href="#" class="text-white">Biblias</a>
+        <a href="#" class="text-white">Biblia em Paralelo</a>
+        <a href="#" class="text-white">Versiculos Tematicos</a>
+      </div>
     </div>
-
-    <div v-if="isMobileMenuOpen" class="md:hidden bg-gray-800 p-4">
-      <!-- Coloque seus itens de menu móvel aqui -->
-      <ul class="space-y-2">
-        <li><a href="#">Item 1</a></li>
-        <li><a href="#">Item 2</a></li>
-        <li><a href="#">Item 3</a></li>
-      </ul>
+    <!-- Menu mobile -->
+    <div v-if="isMobileMenuOpen" class="lg:hidden">
+      <!-- Seus itens de menu aqui -->
+      <a href="#" class="text-white">Homepage</a>
+      <a href="#" class="text-white">Livros</a>
+      <a href="#" class="text-white">Biblias</a>
+      <a href="#" class="text-white">Biblia em Paralelo</a>
+      <a href="#" class="text-white">Versiculos Tematicos</a>
     </div>
   </header>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+
 const isMobileMenuOpen = ref(false)
 
 const toggleMobileMenu = () => {
@@ -46,5 +52,5 @@ const toggleMobileMenu = () => {
 </script>
 
 <style scoped>
-/* Adicione estilos específicos para o cabeçalho aqui */
+/* Seus estilos personalizados aqui */
 </style>
